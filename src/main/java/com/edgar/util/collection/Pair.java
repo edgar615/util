e14505b9e0640847eebabab660fff8dd8a40d327
Edgar Zhang <edgar615@gmail.com>
2016-04-01 22:30:08 +0800

    thread

--- src/main/java/com/edgar/util/collection/Pair.java
+++ src/main/java/com/edgar/util/collection/Pair.java
@@ -26,28 +26,42 @@ public class Pair<A, B> {
     return second;
   }
 
-//  @Override
-//  public boolean equals(Object o) {
-//    if (o == this) { return true; }
-//    if (!(o instanceof Pair)) { return false; }
-//
-//    Pair<?, ?> that = (Pair<?, ?>) o;
-//    return new EqualsBuilder()
-//            .append(this.first, that.first)
-//            .append(this.second, that.second)
-//            .isEquals();
-//  }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    final Pair pair = (Pair) o;
+
+    if (first != null ? !first.equals(pair.first) : pair.first != null) {
+      return false;
+    }
+
+    if (second != null ? !second.equals(pair.second) : pair.second != null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = first != null ? first.hashCode() : 0;
+
+    result = 31 * result + (second != null ? second.hashCode() : 0);
+
+    return result;
+  }
+
 
   @Override
   public String toString() {
     return String.format("(%s, %s)", getFirst(), getSecond());
   }
 
-//  @Override
-//  public int hashCode() {
-//    return new HashCodeBuilder()
-//            .append(first)
-//            .append(second)
-//            .toHashCode();
-//  }
 }

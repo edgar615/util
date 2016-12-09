8d35e6983e0a833252c1aa77d1534fcfbb4d9200
edgar615 <edgar615@gmail.com>
2016-12-09 16:46:09 +0800

    版本判断

---
+++ src/test/java/com/edgar/util/base/VersionUtilsTest.java
@@ -0,0 +1,37 @@
+package com.edgar.util.base;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Edgar on 2016/12/5.
+ *
+ * @author Edgar  Date 2016/12/5
+ */
+public class VersionUtilsTest {
+
+  @Test
+  public void testVersion() {
+    Assert.assertTrue(VersionUtils.compareVersion("V1.0.1", "V1.1") < 0);
+    Assert.assertTrue(VersionUtils.compareVersion("V1.0.1", "1.1") < 0);
+    Assert.assertTrue(VersionUtils.compareVersion("1.0.1", "1.1") < 0);
+    Assert.assertTrue(VersionUtils.compareVersion("1.0.1", "V1.1") < 0);
+
+    Assert.assertTrue(VersionUtils.compareVersion("V1.0.1", "V1.0.0") > 0);
+    Assert.assertTrue(VersionUtils.compareVersion("V1.0.1", "1.0.0") > 0);
+    Assert.assertTrue(VersionUtils.compareVersion("1.0.1", "1.0.0") > 0);
+    Assert.assertTrue(VersionUtils.compareVersion("1.0.1", "1.0.0") > 0);
+
+    Assert.assertTrue(VersionUtils.compareVersion("V1.0", "V1.0.0") == 0);
+    Assert.assertTrue(VersionUtils.compareVersion("V1.0", "1.0.0") == 0);
+    Assert.assertTrue(VersionUtils.compareVersion("1.0", "1.0.0") == 0);
+    Assert.assertTrue(VersionUtils.compareVersion("1.0", "1.0.0") == 0);
+
+    Assert.assertTrue(VersionUtils.compareVersion("V1.0", "V0.9.0") > 0);
+    Assert.assertTrue(VersionUtils.compareVersion("V1.0", "0.9.0") > 0);
+    Assert.assertTrue(VersionUtils.compareVersion("1.0", "0.9.0") > 0);
+    Assert.assertTrue(VersionUtils.compareVersion("1.0", "0.9.0") > 0);
+
+    Assert.assertTrue(VersionUtils.compareVersion("0.6", "0.6") >= 0);
+  }
+}

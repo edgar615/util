27922d32d876b2071ce824b88385e2ba1386623a
edgar615 <edgar615@gmail.com>
2017-02-16 15:49:07 +0800

    错误描述改为英文

--- src/main/java/com/edgar/util/exception/DefaultErrorCode.java
+++ src/main/java/com/edgar/util/exception/DefaultErrorCode.java
@@ -11,146 +11,146 @@ public enum DefaultErrorCode implements ErrorCode {
   /**
    * 空指针异常.
    */
-  UNKOWN(999, "未知错误", 400),
+  UNKOWN(999, "Unknown Error", 400),
 
   /**
    * 空指针异常.
    */
-  NULL(1000, "空指针异常", 400),
+  NULL(1000, "Nullpointer Abnormal", 400),
 
   /**
    * 未知用户.
    */
-  UNKOWN_ACCOUNT(1001, "未知用户", 403),
+  UNKOWN_ACCOUNT(1001, "Unkonwn User", 403),
 
   /**
    * 未登录用户.
    */
-  UNKOWN_LOGIN(1002, "未登录用户", 403),
+  UNKOWN_LOGIN(1002, "User Not Log In", 403),
 
   /**
    * 用户名或密码错误.
    */
-  NAME_PWD_INCORRECT(1003, "用户名或密码错误", 400),
+  NAME_PWD_INCORRECT(1003, "Incorrect Username or Password", 400),
 
   /**
    * 权限不足.
    */
-  NO_AUTHORITY(1004, "权限不足", 403),
+  NO_AUTHORITY(1004, "Insufficient Authority", 403),
 
   /**
    * Token过期.
    */
-  EXPIRE_TOKEN(1005, "Token过期", 403),
+  EXPIRE_TOKEN(1005, "Token Expired", 403),
 
   /**
    * 资源不存在，GET请求是数据不存在.
    */
-  RESOURCE_NOT_FOUND(1006, "资源不存在", 404),
+  RESOURCE_NOT_FOUND(1006, "Resources Not Found", 404),
 
   /**
    * 相关对象不存在，请求依赖的某个对象已经被删除.
    */
-  TARGET_NOT_FOUND(1007, "对象不存在", 400),
+  TARGET_NOT_FOUND(1007, "No Object Found", 400),
 
   /**
    * 参数不全.
    */
-  MISSING_ARGS(1008, "参数不全", 400),
+  MISSING_ARGS(1008, "Incomplete Parameter", 400),
 
   /**
    * 参数非法.
    */
-  INVALID_ARGS(1009, "参数非法", 400),
+  INVALID_ARGS(1009, "Invalid Parameter", 400),
 
   /**
    * 输入为空或字数不够.
    */
-  INPUT_TOO_SHORT(1010, "输入为空或字数不够", 400),
+  INPUT_TOO_SHORT(1010, "Empty or Less Input", 400),
 
   /**
    * 输入字数太多.
    */
-  INPUT_TOO_LONG(1011, "输入的字数过多", 400),
+  INPUT_TOO_LONG(1011, "Over Input", 400),
 
   /**
    * 文件为空.
    */
-  UPLOAD_FILE_EMPTY(1012, "文件为空", 400),
+  UPLOAD_FILE_EMPTY(1012, "Empty File", 400),
 
   /**
    * 上传文件失败.
    */
-  UPLOAD_FILE_FAILED(1013, "上传文件失败", 400),
+  UPLOAD_FILE_FAILED(1013, "Upload Failed", 400),
 
   /**
    * 用户名重复.
    */
-  USERNAME_DUPLICATE(1014, "用户名重复", 400),
+  USERNAME_DUPLICATE(1014, "Username Already Taken", 400),
 
   /**
    * 超时.
    */
-  TIME_OUT(1015, "请求超时", 400),
+  TIME_OUT(1015, "Request Overtime", 400),
 
   /**
    * 未找到远程服务器.
    */
-  UNKOWN_REMOTE(1016, "未找到远程服务器", 400),
+  UNKOWN_REMOTE(1016, "Remote Server Not Found", 400),
 
   /**
    * 参数类型错误.
    */
-  INVALID_TYPE(1017, "参数类型错误", 400),
+  INVALID_TYPE(1017, "Wrong Parameter Format", 400),
 
   /**
    * SQL语句错误.
    */
-  INVALID_SQL(1018, "SQL语句错误", 400),
+  INVALID_SQL(1018, "SQL Error", 400),
 
   /**
    * 类型转换错误.
    */
-  CAST_ERROR(1019, "类型转换错误", 400),
+  CAST_ERROR(1019, "Type Conversion Error", 400),
 
   /**
    * 服务调用异常.
    */
-  REMOTE_EX(1020, "服务调用异常", 400),
+  REMOTE_EX(1020, "Service Exception", 400),
 
   /**
    * TOKEN失效.
    */
-  INVALID_TOKEN(1021, "Token无效", 403),
+  INVALID_TOKEN(1021, "Token Invalid", 403),
 
   /**
    * 非法请求.
    */
-  INVALID_REQ(1022, "非法请求", 400),
+  INVALID_REQ(1022, "Illegal Request", 400),
 
   /**
    * 请求过期.
    */
-  EXPIRE(1023, "过期的请求", 400),
+  EXPIRE(1023, "Request Expired", 400),
 
   /**
    * 非法的JSON.
    */
-  INVALID_JSON(1024, "错误的JSON格式", 400),
+  INVALID_JSON(1024, "Wrong JSON Format", 400),
   /**
    * 事件超时.
    */
-  EVENTBUS_TIMOUT(1025, "事件超时", 400),
+  EVENTBUS_TIMOUT(1025, "Eventbus: Timeout", 400),
 
   /**
    * 事件被拒绝.
    */
-  EVENTBUS_REJECTED(1026, "事件被拒绝", 400),
+  EVENTBUS_REJECTED(1026, "Eventbus: Rejected", 400),
 
   /**
    * 未定义事件.
    */
-  EVENTBUS_NO_HANDLERS(1027, "未定义事件", 400);
+  EVENTBUS_NO_HANDLERS(1027, "Eventbus: Undefined Event", 400);
 
   /**
    * 异常编码.

9044788cede7041ac70a852ae515bc152fab60e5
edgar615 <edgar615@gmail.com>
2017-06-11 10:05:48 +0800

    loadbalance

---
+++ src/test/java/com/edgar/util/loadbalance/StrategyTest.java
@@ -0,0 +1,31 @@
+package com.edgar.util.loadbalance;
+
+import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by edgar on 17-5-6.
+ */
+public class StrategyTest {
+  List<ServiceInstance> instances = new ArrayList<>();
+
+  @Before
+  public void setUp() {
+    instances.clear();
+    instances.add(new ServiceInstance("a", "test"));
+    instances.add(new ServiceInstance("b", "test"));
+    instances.add(new ServiceInstance("c", "test"));
+  }
+
+  protected List<String> select3000(ProviderStrategy strategy) {
+    List<String> selected = new ArrayList<>();
+    for (int i = 0; i < 3000; i++) {
+      ServiceInstance instance = strategy.get(instances);
+      selected.add(instance.id());
+    }
+    return selected;
+  }
+
+}

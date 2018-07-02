47b27482549f1de45ecf4d4b14e7bd9d4835e266
edgar615 <edgar615@gmail.com>
2018-07-02 14:12:46 +0800

    AES

--- src/main/java/com/github/edgar615/util/base/EncryptUtils.java
+++ src/main/java/com/github/edgar615/util/base/EncryptUtils.java
@@ -121,7 +121,6 @@ public class EncryptUtils {
     return byte2hex(md5.digest());
   }
 
-
   public static String byte2hex(byte[] bytes) {
     StringBuilder sign = new StringBuilder();
     for (int i = 0; i < bytes.length; i++) {
@@ -132,5 +131,16 @@ public class EncryptUtils {
       sign.append(hex.toUpperCase());
     }
     return sign.toString();
+  }
+
+  public static byte[] hex2byte(String hexStr) {
+    if (hexStr.length() < 1) { return null; }
+    byte[] result = new byte[hexStr.length() / 2];
+    for (int i = 0; i < hexStr.length() / 2; i++) {
+      int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+      int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
+      result[i] = (byte) (high * 16 + low);
+    }
+    return result;
   }
 }
